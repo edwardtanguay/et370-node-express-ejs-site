c6137c407550977d935d07d2c436c63a83bd1c53
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-17 01:39:53 +0100

    loading async

---
+++ src/interfaces.ts
@@ -0,0 +1,5 @@
+export interface IBook {
+	idCode: string;
+	title: string;
+	description: string;
+}
--- src/model.ts
+++ src/model.ts
@@ -1,13 +1,24 @@
-export const getBooks = () => {
-	return [
-		{
-			title: "Book 1"
-		},
-		{
-			title: "Book 2"
-		},
-		{
-			title: "Book 3"
+import axios from 'axios';
+import { IBook } from './interfaces';
+
+export const getBooks = async () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const response = await axios.get('https://edwardtanguay.vercel.app/share/books.json');
+			const rawBooks: any[] = response.data;
+			const books: IBook[] = [];
+			for (const rawBook of rawBooks) {
+				const book: IBook = {
+					idCode: rawBook.idcode,
+					title: rawBook.title,
+					description: rawBook.description
+				}
+				books.push(book);
+			}
+			resolve(books);
 		}
-	]
+		catch (e) {
+			reject({ message: "unknown error occurred" })
+		}
+	});
 }
